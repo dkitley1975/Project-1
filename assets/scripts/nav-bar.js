1446da6f05b3c6eb8fb6666d8d5a584ea61a9767
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-07-07 10:36:39 +0100

    Added underline to active section on menu and Video posters

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -14,3 +14,24 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", navHighlighter);
+function navHighlighter() {
+  let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    )
+    {
+      document.querySelector(".nav-menu a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".nav-menu a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
+
