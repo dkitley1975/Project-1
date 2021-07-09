336a69fda17cb6e1df5c9c5d0fd5d89801f58c13
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-07-09 11:36:28 +0100

    removed unnecessary comments

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -6,32 +6,31 @@ hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
 }
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
 }
 
 const sections = document.querySelectorAll("section[id]");
 window.addEventListener("scroll", navHighlighter);
-function navHighlighter() {
-  let scrollY = window.pageYOffset;
-    sections.forEach(current => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop -5;
-    sectionId = current.getAttribute("id");
-    if (
-      scrollY > sectionTop &&
-      scrollY <= sectionTop + sectionHeight
-    )
-    {
-      document.querySelector(".nav-menu a[href*=" + sectionId + "]").classList.add("active");
-    } else {
-      document.querySelector(".nav-menu a[href*=" + sectionId + "]").classList.remove("active");
-    }
-  });
-}
 
+function navHighlighter() {
+	let scrollY = window.pageYOffset;
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 5;
+		sectionId = current.getAttribute("id");
+		if (
+			scrollY > sectionTop &&
+			scrollY <= sectionTop + sectionHeight
+		) {
+			document.querySelector(".nav-menu a[href*=" + sectionId + "]").classList.add("active");
+		} else {
+			document.querySelector(".nav-menu a[href*=" + sectionId + "]").classList.remove("active");
+		}
+	});
+}
--- assets/scripts/newsletter_signup.js
+++ assets/scripts/newsletter_signup.js
@@ -1,19 +1,19 @@
-window.onload = function() {
-    document.getElementById('newsletter-signup-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.ident_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('service_dssf123', 'template_vywdbyb', this)
-            .then(function() {
-                console.log('SUCCESS!');
-                document.getElementById("newsletter-signup-form").reset();
-                document.getElementById("newsletter-signup-form").style.display="none";
-                document.getElementById("newsletter-signup-form-thank-you").style.display="block";
-            }, function(error) {
-                console.log('FAILED...', error)
-                document.getElementById("newsletter-signup-form-thank-you").style.display="none";
-            });
-    });
-    
+window.onload = function () {
+	document.getElementById('newsletter-signup-form').addEventListener('submit', function (event) {
+		event.preventDefault();
+		// generate a five digit number for the contact_number variable
+		this.ident_number.value = Math.random() * 100000 | 0;
+		// these IDs from the previous steps
+		emailjs.sendForm('service_dssf123', 'template_vywdbyb', this)
+			.then(function () {
+				console.log('SUCCESS!');
+				document.getElementById("newsletter-signup-form").reset();
+				document.getElementById("newsletter-signup-form").style.display = "none";
+				document.getElementById("newsletter-signup-form-thank-you").style.display = "block";
+			}, function (error) {
+				console.log('FAILED...', error)
+				document.getElementById("newsletter-signup-form-thank-you").style.display = "none";
+			});
+	});
+
 }
